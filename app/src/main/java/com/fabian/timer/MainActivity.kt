be1da5fc07dfde88c044fbb5c1375abc7777efcb
fabian4 <fabian4@163.com>
2021-10-12 00:28:23 +0800

    绘制页面

--- app/src/main/java/com/fabian/timer/MainActivity.kt
+++ app/src/main/java/com/fabian/timer/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.fabian.timer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer = findViewById<TextView>(R.id.timer)
+        timer.typeface = Typeface.createFromAsset(assets, "fonts/ds_digi.ttf")
+        timer.text = "23:22"
+        timer.textSize = 250f
     }
 }
