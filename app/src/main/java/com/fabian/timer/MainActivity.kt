da7990bbc4ce5434924e7ba61237ddb6d0fa4e54
fabian4 <fabian4@163.com>
2021-10-12 23:51:06 +0800

    逻辑完善

--- app/src/main/java/com/fabian/timer/MainActivity.kt
+++ app/src/main/java/com/fabian/timer/MainActivity.kt
@@ -7,10 +7,7 @@ import android.content.IntentFilter
 import android.icu.util.Calendar
 import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.fabian.timer.flip.FlipDigit
@@ -18,54 +15,58 @@ import com.fabian.timer.flip.FlipDigit
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var hours:TextView
-    private lateinit var minutes:TextView
-    private lateinit var filipSingle:FlipDigit
+    private lateinit var hours1:FlipDigit
+    private lateinit var hours2:FlipDigit
+    private lateinit var minutes1:FlipDigit
+    private lateinit var minutes2:FlipDigit
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val filter = IntentFilter()
-//        filter.addAction(Intent.ACTION_TIME_TICK)
-//        val receiver = TimeChangerReceiver()
-//        registerReceiver(receiver, filter)
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_TIME_TICK)
+        val receiver = TimeChangerReceiver()
+        registerReceiver(receiver, filter)
 
-        //屏幕常亮设置
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-//        hours = findViewById(R.id.hours)
-//        minutes = findViewById(R.id.minutes)
-//
-//        val c: Calendar = Calendar.getInstance()
-//        hours.text = c.get(Calendar.HOUR_OF_DAY).toString().fix()
-//        minutes.text = c.get(Calendar.MINUTE).toString().fix()
+        hours1 = findViewById(R.id.hours_1)
+        hours2 = findViewById(R.id.hours_2)
+        minutes1 = findViewById(R.id.minutes_1)
+        minutes2 = findViewById(R.id.minutes_2)
 
-        filipSingle = findViewById(R.id.filipSingle)
-        filipSingle.setDigit(3,true)
-        Thread {
-            for (index in 0..9){
-                Thread.sleep(5000)
-                filipSingle.setDigit(index, true)
-            }
-        }.start()
+        update()
+
     }
 
-//    fun String.fix():String{
-//        if(this.length == 1){
-//            return "0$this"
-//        }
-//        return this
-//    }
-//
-//    inner class TimeChangerReceiver:BroadcastReceiver(){
-//        @RequiresApi(Build.VERSION_CODES.N)
-//        override fun onReceive(context: Context?, intent: Intent?) {
-//            val c: Calendar = Calendar.getInstance()
-//            hours.text = c.get(Calendar.HOUR_OF_DAY).toString().fix()
-//            minutes.text = c.get(Calendar.MINUTE).toString().fix()
-//        }
-//    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun update(){
+        val c: Calendar = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY).toString()
+        if (hour.length == 1){
+            hours1.setDigit(0, true)
+            hours2.setDigit(Integer.parseInt(hour), true)
+        }else{
+            hours1.setDigit(Integer.parseInt(hour[0].toString()), true)
+            hours2.setDigit(Integer.parseInt(hour[1].toString()), true)
+        }
+        val minute = c.get(Calendar.MINUTE).toString()
+        if (minute.length == 1){
+            minutes1.setDigit(0, true)
+            minutes2.setDigit(Integer.parseInt(minute), true)
+        }else{
+            minutes1.setDigit(Integer.parseInt(minute[0].toString()), true)
+            minutes2.setDigit(Integer.parseInt(minute[1].toString()), true)
+        }
+    }
+
+    inner class TimeChangerReceiver:BroadcastReceiver(){
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onReceive(context: Context?, intent: Intent?) {
+            update()
+        }
+    }
 
 
 }
